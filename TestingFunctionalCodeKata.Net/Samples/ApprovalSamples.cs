89342ffbd3ff69609ccf02e80d747307abd10527
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-12-06 13:26:52 +0000

    t sin

---
+++ TestingFunctionalCodeKata.Net/Samples/ApprovalSamples.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingFunctionalCodeKata.Net.Samples
+{
+    [TestClass]
+    [UseReporter(typeof(DiffReporter))]
+    public class ApprovalSamples
+    {
+        [TestMethod]
+        public void TestVerifyAll()
+        {
+            Approvals.VerifyAll("sin", new[] { 0.1 }, d => $"sin({d}) = {TrigMath.Sin(d)} ");
+        }
+    }
+}
